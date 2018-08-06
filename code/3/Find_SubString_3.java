72f1723c5f6ecdb9c1efaba23f0c46a1722e9b31
Harry_Sun <hacksun@126.com>
2018-08-06 20:33:09 +0800

    8.6

---
+++ code/3/Find_SubString_3.java
@@ -0,0 +1,21 @@
+class solution{
+	
+	public void findSubString(String in){
+		int length = in.length(in)-1;
+		for(int i = 0;i<=length;i++){
+			if(in.charAt(i)=in.charAt(i+1)){
+				
+			}
+		}
+	}
+}
+
+
+
+public class Find_SubString_3{
+	public static void main(String[] args){
+		String in  = new String("abcabcbb");
+		solution ex = new solution();
+		ex.findSubString(in);
+	}
+}
